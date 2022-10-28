c3967a94e2d0d375f17611482a41105df8c2b857
Chris McKnight <tenlongfingers10@gmail.com>
2022-10-27 20:24:40 -0600

    saving before my computer crashes because it's definitely gonna happen son

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -1,16 +1,25 @@
-import setText, {appendText, showWaiting, hideWaiting} from "./results.mjs";
+import setText, { appendText, showWaiting, hideWaiting } from "./results.mjs";
 
 export function get() {
+  axios.get("http://localhost:3000/orders/1").then(({ data }) => {
+    setText(JSON.stringify(data));
+  });
 }
 
 export function getCatch() {
+  axios
+    .get("http://localhost:3000/orders/123")
+    .then(({ data }) => {
+      setText(JSON.stringify(data));
+    })
+    .catch((err) => setText(err));
 }
 
-export function chain() {
-}
+export function chain() {}
+axios.get("http://localhost:3000/orders/1").then(({ data }) => {
+  setText(JSON.stringify(data));
+});
 
-export function chainCatch() {
-}
+export function chainCatch() {}
 
-export function final() {
-}
+export function final() {}
